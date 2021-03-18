5ffa343908bac4cc4035e87c73cdc940382a9540
baxodir <boxa.developer@gmail.com>
2021-03-18 16:52:54 +0500

    Added Tracker

---
+++ detector.py
@@ -0,0 +1,98 @@
+import cv2
+import numpy as np
+import timeit
+
+
+# import myTracker
+
+
+class Detector:
+    def __init__(self):
+        super().__init__()
+        self.net = None
+        self.output_layers = None
+        self.setModels()
+        self.classes = []
+        with open("./yolo_weights/coco.names", "r") as f:
+            self.classes = [line.strip() for line in f.readlines()]
+
+    def setModels(self):
+        print('SetModel:::::::::')
+        net = cv2.dnn.readNet("./yolo_weights/yolov3-full.weights", "./yolo_weights/yolov3-full.cfg")
+        net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+        net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+        layer_names = net.getLayerNames()
+        output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+        self.net = net
+        self.output_layers = output_layers
+
+    def drawBoxes(self, frame):
+        st = cv2.getTickCount()
+        cv2.putText(frame, 'Detected Cars:  ', (250, 1210), cv2.FONT_HERSHEY_SIMPLEX, 2.75,
+                    (0, 0, 255), 4)
+        height, width, channels = frame.shape
+        net, output_layers = self.net, self.output_layers
+        # 0.00392
+        blob = cv2.dnn.blobFromImage(frame, 0.00392, (256, 256), (0, 0, 0), True, crop=False)
+        net.setInput(blob)
+
+        outs = net.forward(output_layers)
+
+        class_ids = []
+        confidences = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:
+                    # Object detected
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+
+                    # Rectangle coordinates
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        # print([box.append(indexes[i]) for i, box in enumerate(boxes)])
+        colors = np.random.uniform(0, 255, size=(len(boxes), 3))
+        dx = 30
+        # print(self.classes)
+        # temp = self.cntObj.update(boxes)
+        dets = []
+        for i in range(len(boxes)):
+            if i in indexes:
+                label = str(self.classes[class_ids[i]])
+                if label == 'car':
+                    dets.append(boxes[i] + [i])
+                    # print(boxes[i]+[i])
+                    # x, y, w, h = boxes[i]
+                    #
+                    # color = colors[i]
+                    #
+                    # sX = x + w // 2 - dx
+                    # sY = y + h // 2 - dx
+                    # eX = x + w // 2 + dx
+                    # eY = y + h // 2 + dx
+                    #
+                    # # cv2.rectangle(frame, (sX, sY), (eX, eY), color, 1)
+                    # cv2.rectangle(frame, (x, y), (x + w, y + h), color, 1)
+                    # cv2.putText(frame, str(label), (sX, sY), cv2.FONT_HERSHEY_SIMPLEX, 1.75,
+                    #             (0, 255, 0), 1)
+                    # cv2.putText(frame, 'Detected Cars: {}'.format(str(len(indexes))), (250, 1210),
+                    #             cv2.FONT_HERSHEY_SIMPLEX, 1,
+                    #             (0, 0, 255), 2)
+                    # # print()
+                    # fps = cv2.getTickFrequency() / (cv2.getTickCount() - st)
+                    # cv2.putText(frame, 'FPS: {}'.format(str(round(fps, 2))), (250, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255),
+                    #             2)
+                    # cv2.imshow("Frame", frame)
+                    # cv2.waitKey(1)
+        return dets
---
+++ tracker.py
@@ -0,0 +1,68 @@
+from sort import *
+
+sort_tracker = Sort()
+#
+#
+# # update SORT
+
+import cv2
+import threading
+import time, timeit
+import detector
+
+url = 'rtsp://admin:parol12345@192.168.4.220:554/cam/realmonitor?channel=1&subtype=0'
+# url = 'rtsp://admin:Aa123456@10.4.38.12:554/StreamingSetting?version=1.0&action=getRTSPStream&ChannelID=1&ChannelName=Channel1'
+# url = 'traffic2.mp4'
+getFrame = True
+frame = None
+cv2.namedWindow("w", cv2.WINDOW_KEEPRATIO)
+detector_ = detector.Detector()
+
+
+def drawLimit():
+    global frame
+    # w,h = frame.shape[:2]
+    # cv2.line(frame,(0,h),(w,h),(0,0,255),10)
+    # cv2.line(frame, (0, 231), (w, 231), (0, 0, 255), 10)
+
+
+def detectCar():
+    global frame, detector_
+    timer = cv2.getTickCount()
+    dets = detector_.drawBoxes(frame)
+    # print(dets)
+    track_bbox_ids = sort_tracker.update(np.array(dets))
+    print(track_bbox_ids)
+    # frame = detector_.drawBoxes(frame)
+    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+    cv2.putText(frame, "FPS : " + str(int(fps)), (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+
+
+def getCap():
+    global getFrame, frame
+    cap = cv2.VideoCapture(url)
+    while True:
+        if cap.grab():
+            if getFrame:
+                r, frame = cap.retrieve()
+                # frame = cv2.resize(frame,(1000,500))
+                # drawLimit()
+                getFrame = False
+
+
+def showCap():
+    global getFrame, frame
+    while True:
+        if not getFrame:
+            detectCar()
+            cv2.imshow('w', frame)
+            getFrame = True
+
+
+x1 = threading.Thread(target=getCap)
+x1.start()
+time.sleep(2)
+x2 = threading.Thread(target=showCap)
+x2.start()
+
+cv2.waitKey(0)
